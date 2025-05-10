7035e8ac1eac89d52bd45a6cd43841a9099f9d7f
Ismael Bueno <ismaelwno@gmail.com>
2025-05-10 16:47:34 +0200

    Tweak fugitive bindings

--- lua/ismawno/lazy/fugitive.lua
+++ lua/ismawno/lazy/fugitive.lua
@@ -16,14 +16,10 @@ return {
 
                 local bufnr = vim.api.nvim_get_current_buf()
                 local opts = { buffer = bufnr, remap = false }
-                vim.keymap.set('n', '<leader>p', function()
-                    vim.cmd.Git('push')
-                end, opts)
-
-                -- rebase always
-                vim.keymap.set('n', '<leader>P', function()
-                    vim.cmd.Git({ 'pull', '--rebase' })
-                end, opts)
+                vim.keymap.set('n', '<leader>ga', ':G add .<CR>')
+                vim.keymap.set('n', '<leader>gc', ':G commit<CR>')
+                vim.keymap.set('n', '<leader>gp', ':G push<CR>')
+                vim.keymap.set('n', '<leader>gP', ':G pull<CR>')
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
