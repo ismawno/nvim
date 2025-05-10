aedc2c47d263f805446e69fa5fde16db6a9e0f38
Ismael Bueno <ismaelwno@gmail.com>
2025-05-10 18:34:51 +0200

    Add grep git files

--- lua/ismawno/lazy/telescope.lua
+++ lua/ismawno/lazy/telescope.lua
@@ -9,24 +9,49 @@ return {
 
     config = function()
         require('telescope').setup({})
+        local function grep_git_files(word)
+            require('telescope.builtin').grep_string({
+                search = word,
+                search_dirs = vim.fn.systemlist('git ls-files'),
+            })
+        end
 
         local builtin = require('telescope.builtin')
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
+        vim.keymap.set('n', '<leader>gpf', builtin.git_files, {})
+
+        vim.keymap.set('n', '<leader>pw', function()
             local word = vim.fn.expand('<cword>')
             builtin.grep_string({ search = word })
         end)
-        vim.keymap.set('n', '<leader>pWs', function()
+        vim.keymap.set('n', '<leader>gpw', function()
+            local word = vim.fn.expand('<cword>')
+            grep_git_files(word)
+        end)
+
+        vim.keymap.set('n', '<leader>pW', function()
             local word = vim.fn.expand('<cWORD>')
             builtin.grep_string({ search = word })
         end)
+        vim.keymap.set('n', '<leader>gpW', function()
+            local word = vim.fn.expand('<cWORD>')
+            grep_git_files(word)
+        end)
+
         vim.keymap.set('n', '<leader>pg', function()
             builtin.grep_string({ search = vim.fn.input('Grep > ') })
         end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string()
+        vim.keymap.set('n', '<leader>gpg', function()
+            grep_git_files(vim.fn.input('Grep > '))
         end)
+
+        vim.keymap.set('n', '<leader>ps', function()
+            builtin.grep_string({ search = '' })
+        end)
+        vim.keymap.set('n', '<leader>gps', function()
+            grep_git_files('')
+        end)
+
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
     end,
 }
