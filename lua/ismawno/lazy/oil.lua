885479d141e697aab370008ab87346d92083fde4
Ismael Bueno <ismaelwno@gmail.com>
2025-05-22 08:01:47 +0200

    Use git when possible with oil

--- lua/ismawno/lazy/oil.lua
+++ lua/ismawno/lazy/oil.lua
@@ -1,3 +1,8 @@
+local function in_git_repo()
+    -- returns true if the current buffer is in a git worktree
+    return vim.fn.systemlist('git rev-parse --is-inside-work-tree')[1] == 'true'
+end
+
 return {
     'stevearc/oil.nvim',
     ---@module 'oil'
@@ -8,5 +13,21 @@ return {
     -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
     -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
     lazy = false,
-    opts = { use_default_keymaps = true, view_options = { show_hidden = true } },
+    opts = {
+        use_default_keymaps = true,
+        view_options = { show_hidden = true },
+        git = { -- enable `git add` on new files
+            add = function(path)
+                return in_git_repo()
+            end,
+            -- enable `git mv` on renames
+            mv = function(src, dest)
+                return in_git_repo()
+            end,
+            -- enable `git rm` on deletes
+            rm = function(path)
+                return in_git_repo()
+            end,
+        },
+    },
 }
