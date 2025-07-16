a27fa926277ddced6a47c5279a9c7db57b4daa9e
Ismael Bueno <ismaelwno@gmail.com>
2025-07-16 15:23:37 +0100

    Add copilot

---
+++ lua/ismawno/lazy/copilot.lua
@@ -0,0 +1,26 @@
+return {
+    'zbirenbaum/copilot.lua',
+    event = 'InsertEnter',
+    config = function()
+        require('copilot').setup({
+            suggestion = {
+                enabled = true,
+                auto_trigger = true,
+                debounce = 75,
+                keymap = {
+                    accept = '<C-u>',
+                    next = '<C-n>',
+                    prev = '<C-p>',
+                    dismiss = '<C-m>',
+                },
+            },
+            panel = {
+                enabled = true,
+                auto_refresh = false,
+                keymap = {
+                    open = '<C-e>',
+                },
+            },
+        })
+    end,
+}
