9b220a45b5ab2b2c5a71caf27fabeebb0d921aa0
Ismael Bueno <ismaelwno@gmail.com>
2025-07-17 15:55:07 +0100

    Add git status to oil.nvim

--- lua/ismawno/lazy/oil.lua
+++ lua/ismawno/lazy/oil.lua
@@ -4,46 +4,58 @@ local function in_git_repo()
 end
 
 return {
-    'stevearc/oil.nvim',
-    ---@module 'oil'
-    ---@type oil.SetupOpts
-    -- Optional dependencies
-    dependencies = {
-        {
-            'echasnovski/mini.icons',
-            priority = 1002,
-            lazy = false,
-            config = function()
-                require('mini.icons').mock_nvim_web_devicons()
-            end,
+    {
+        'stevearc/oil.nvim',
+        ---@module 'oil'
+        ---@type oil.SetupOpts
+        -- Optional dependencies
+        dependencies = {
+            {
+                'echasnovski/mini.icons',
+                priority = 1002,
+                lazy = false,
+                config = function()
+                    require('mini.icons').mock_nvim_web_devicons()
+                end,
+            },
+        },
+        --
+        -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
+        -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+        lazy = false,
+        opts = {
+            win_options = { signcolumn = 'yes:2' },
+            keymaps = {
+                ['<leader>vs'] = 'actions.select_vsplit',
+                ['<leader>sp'] = 'actions.select_split',
+                ['<C-s>'] = false,
+                ['<C-h>'] = false,
+                ['<C-t>'] = false,
+            },
+            use_default_keymaps = true,
+            view_options = { show_hidden = true },
+            git = { -- enable `git add` on new files
+                add = function()
+                    return in_git_repo()
+                end,
+                -- enable `git mv` on renames
+                mv = function()
+                    return in_git_repo()
+                end,
+                -- enable `git rm` on deletes
+                rm = function()
+                    return in_git_repo()
+                end,
+            },
         },
     },
-    --
-    -- dependencies = { 'nvim-tree/nvim-web-devicons' }, -- use if you prefer nvim-web-devicons
-    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-    lazy = false,
-    opts = {
-        keymaps = {
-            ['<leader>vs'] = 'actions.select_vsplit',
-            ['<leader>sp'] = 'actions.select_split',
-            ['<C-s>'] = false,
-            ['<C-h>'] = false,
-            ['<C-t>'] = false,
+    {
+        'refractalize/oil-git-status.nvim',
+
+        dependencies = {
+            'stevearc/oil.nvim',
         },
-        use_default_keymaps = true,
-        view_options = { show_hidden = true },
-        git = { -- enable `git add` on new files
-            add = function()
-                return in_git_repo()
-            end,
-            -- enable `git mv` on renames
-            mv = function()
-                return in_git_repo()
-            end,
-            -- enable `git rm` on deletes
-            rm = function()
-                return in_git_repo()
-            end,
-        },
+
+        config = true,
     },
 }
