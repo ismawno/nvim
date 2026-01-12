2bac4bf828fe77bfcda018e8336829091e5f695c
Ismael Bueno <ismaelwno@gmail.com>
2026-01-12 19:13:53 +0100

    chore: Reduce side scrolloff

--- lua/ismawno/set.lua
+++ lua/ismawno/set.lua
@@ -25,7 +25,7 @@ vim.opt.incsearch = true
 -- vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 32
+vim.opt.sidescrolloff = 16
 vim.opt.signcolumn = 'yes'
 vim.opt.isfname:append('@-@')
 
