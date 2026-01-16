189efb121be734c6cd166cb1f2e360c2938d84b2
Ismael Bueno <ismaelwno@gmail.com>
2026-01-16 19:37:42 +0100

    chore: Remove more scrolloff

--- lua/ismawno/set.lua
+++ lua/ismawno/set.lua
@@ -25,7 +25,7 @@ vim.opt.incsearch = true
 -- vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
-vim.opt.sidescrolloff = 16
+vim.opt.sidescrolloff = 4
 vim.opt.signcolumn = 'yes'
 vim.opt.isfname:append('@-@')
 
