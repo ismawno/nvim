07a847fc15264f002aaadb6b1b9abbf13c9d6bfb
Ismael Bueno <ismaelwno@gmail.com>
2026-08-12 19:39:40 +0200

    feat: Add nvim lint

---
+++ lua/ismawno/lazy/lint.lua
@@ -0,0 +1,14 @@
+return {
+    'mfussenegger/nvim-lint',
+    config = function()
+        require('lint').linters_by_ft = {
+            cpp = { 'clangtidy' },
+            c = { 'clangtidy' },
+        }
+        vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost' }, {
+            callback = function()
+                require('lint').try_lint()
+            end,
+        })
+    end,
+}
