e661223d5817afd7b3181e8ccf02c62ed8ae8609
Ismael Bueno <ismaelwno@gmail.com>
2026-08-12 19:40:33 +0200

    chore: Update ft

--- lua/ismawno/lazy/lint.lua
+++ lua/ismawno/lazy/lint.lua
@@ -3,7 +3,9 @@ return {
     config = function()
         require('lint').linters_by_ft = {
             cpp = { 'clangtidy' },
+            hpp = { 'clangtidy' },
             c = { 'clangtidy' },
+            h = { 'clangtidy' },
         }
         vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost' }, {
             callback = function()
