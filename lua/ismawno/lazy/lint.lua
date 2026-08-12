4a192a1acdaaa68fa7201c950f10fecdb8fcd475
Ismael Bueno <ismaelwno@gmail.com>
2026-08-12 19:43:52 +0200

    chore: Specify compile cmds

--- lua/ismawno/lazy/lint.lua
+++ lua/ismawno/lazy/lint.lua
@@ -1,12 +1,14 @@
 return {
     'mfussenegger/nvim-lint',
     config = function()
-        require('lint').linters_by_ft = {
-            cpp = { 'clangtidy' },
-            hpp = { 'clangtidy' },
-            c = { 'clangtidy' },
-            h = { 'clangtidy' },
+        local lint = require('lint')
+        local root = require('ismawno.utils').find_root()
+        lint.linters.clangtidy.args = {
+            '-p',
+            root .. 'build',
         }
+
+        lint.linters_by_ft = { cpp = { 'clangtidy' }, hpp = { 'clangtidy' }, c = { 'clangtidy' }, h = { 'clangtidy' } }
         vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufReadPost' }, {
             callback = function()
                 require('lint').try_lint()
