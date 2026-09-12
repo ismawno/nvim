d173978d85fd68052e9d7b7e3852d2be437143d5
Ismael Bueno <ismaelwno@gmail.com>
2026-09-12 19:05:13 +0200

    chore: Update blink

--- lua/ismawno/lazy/lsp.lua
+++ lua/ismawno/lazy/lsp.lua
@@ -4,7 +4,7 @@ local dependencies = {
     'stevearc/conform.nvim',
     {
         'saghen/blink.cmp',
-        version = '1.4.1',
+        version = '1.10.2',
         opts = {
             appearance = {
                 nerd_font_variant = 'mono',
